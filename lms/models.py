2e0e494be62a83d627edaffa4191e0e81e0300a5
rybin <rybin.32@gmail.com>
2025-12-07 22:45:11 +0300

    HW is done

---
+++ lms/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название курса",
+        help_text="Укажите название курса",
+    )
+    preview = models.ImageField(
+        upload_to="courses/image", help_text="Добавьте заставку для курса", blank=True, null=True
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="описание курса",
+        help_text="Добавьте описание",
+    )
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название урока",
+        help_text="Введите название урока",
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="описание урока",
+        help_text="Добавьте описание",
+    )
+    preview = models.ImageField(
+        blank=True, null=True, help_text="Добавьте заставку для урока"
+    )
+    url = models.URLField(
+        blank=True,
+        null=True,
+        verbose_name="ссылка на урок",
+        help_text="укажите ссылку на урок",
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        verbose_name="Название курса",
+        help_text="Выберите, к какому курсу относится урок",
+    )
